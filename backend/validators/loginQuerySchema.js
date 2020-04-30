5217014c5102fe244614d71df36959c7722a83bb
Philippe Talarico <p.talarico20@gmail.com>
2020-04-30 18:39:25 +0200

    Delete loginQuerySchema.js

--- backend/validators/loginQuerySchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const loginQuerySchema = Joi.object({
-    username: Joi.string().min(6).max(30).required(),
-    password: Joi.string().min(8).max(64).required(),
-});
-
-module.exports = loginQuerySchema;
